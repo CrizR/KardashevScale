66b897daf389c8e992c08d8323c15b87496e779b
Chris Risley <risleychris@gmail.com>
2026-07-26 08:55:29 -0400

    Remove legacy router and autoplay audio

--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
 import React from 'react';
-import './index.css';
-import Calculator from "./components/Calculator/Calculator";
-import {BrowserRouter as Router, Route} from "react-router-dom";
-import {Switch} from "react-bootstrap";
-import LifeScale from "./components/LifeScale/LifeScale";
-import mp3_file from "./music.mp3"
-
+import Calculator from './components/Calculator/Calculator';
+import LifeScale from './components/LifeScale/LifeScale';
 
 function App() {
+  const pathname = window.location.pathname.replace(/\/+$/, '') || '/';
 
-    return (
-        <>
-            <audio
-                autoPlay={true}>
-                <source type="audio/mp3" src={mp3_file}/>
-            </audio>
-            <div className="App">
-                <Router>
-                    <Switch>
-                        <Route path="/calculator" exact component={Calculator}/>
-                        <Route path="/" exact component={LifeScale}/>
-                    </Switch>
-                </Router>
-            </div>
-        </>
-    );
+  if (pathname === '/calculator') {
+    return <Calculator />;
+  }
+
+  return <LifeScale />;
 }
 
 export default App;
