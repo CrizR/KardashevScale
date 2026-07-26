5a4b7450a8fa5c6174dce8d61db4e1685b318602
Chris Risley <risleychris@gmail.com>
2026-07-26 09:11:01 -0400

    Compile legacy JSX stored in JavaScript files

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  oxc: {
+    include: /src\/.*\.js$/,
+    jsx: {
+      runtime: 'automatic',
+    },
+  },
   build: {
     target: 'es2020',
     sourcemap: false,
