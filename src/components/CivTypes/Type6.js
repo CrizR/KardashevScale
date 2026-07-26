8c9a6e33b0d4e27cf4100787c7ac82675b6bd77e
Chris Risley <risleychris@gmail.com>
2026-07-26 09:00:30 -0400

    Fix Type VI rating display

--- src/components/CivTypes/Type6.js
+++ src/components/CivTypes/Type6.js
@@ -1,18 +1,21 @@
-import React, {Component} from "react";
-import * as PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
-export default class Type6 extends Component {
-    render() {
-        return <div className={"civStyle"}>
-            <h2>Type VI Civilization {this.props.showTier ? - this.props.tier : null}</h2>
-            <h3>Masters of Space of Time</h3>
-            <p>Exists outside of time and space, and is capable of creating universes and multiverses, and
-                destroying them just as easily.</p>
-            <ul>
-                <li>You are a god</li>
-            </ul>
-        </div>;
-    }
+export default function Type6({ tier = 6, showTier = false }) {
+  return (
+    <div className="civStyle">
+      <h2>Type VI Civilization {showTier ? <span className="rating">K {tier.toFixed(3)}</span> : null}</h2>
+      <h3>Masters of Space-Time</h3>
+      <p>A terminal speculative category describing control over space-time and universe creation itself.</p>
+      <ul>
+        <li>Not part of Kardashev's original proposal</li>
+        <li>Useful as science-fiction extrapolation, not an observational classification</li>
+      </ul>
+    </div>
+  );
 }
 
-Type6.propTypes = {tier: PropTypes.number};
+Type6.propTypes = {
+  tier: PropTypes.number,
+  showTier: PropTypes.bool,
+};
